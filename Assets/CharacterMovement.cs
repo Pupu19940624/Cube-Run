ecab97f39d8801c56c149406b1d5a999acd21323
kevin940726 <kevin830726@gmail.com>
2015-11-19 00:38:52 +0800

    add character controller, jump height

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -6,15 +6,20 @@ using UnityStandardAssets.CrossPlatformInput;
 public class CharacterMovement : MonoBehaviour {
 
 	public float movementSpeed = 5.0f;
-	public float jumpSpeed = 3.0f;
+	public float jumpSpeed = 5.0f;
+	public float gravity = 2.0f;
 	Animator animator;
 	float speed = 0.0f;
 	float maxSpeed = 1.0f;
 	float minSpeed = 0.0f;
+	Vector3 moveToward;
+	CharacterController characterController;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+		moveToward = Vector3.zero;
+		characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -25,28 +30,32 @@ public class CharacterMovement : MonoBehaviour {
 		// Vector3 direction = Math.Abs(horizontal) > Math.Abs(vertical) ? new Vector3(1, 0, 0) * horizontal : new Vector3(0, 1, 0) * vertical;
 		// gameObject.transform.Translate(direction * movementSpeed * Time.deltaTime);
 
-		//// 360 degree
-		if (CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0) {
-			speed = Mathf.Clamp(speed + Time.deltaTime, minSpeed, maxSpeed);
-			animator.SetFloat("WalkSpeed", speed);
-		}
-		else {
-			speed = Mathf.Clamp(speed - Time.deltaTime, minSpeed, maxSpeed);
-			animator.SetFloat("WalkSpeed", speed);
-		}
-
-		if (CrossPlatformInputManager.GetButtonDown("Jump")) {
-			animator.SetTrigger("Jump");
-			speed = 0;
-			animator.SetFloat("WalkSpeed", speed);
-		}
-
-		Vector3 moveToward = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
-		gameObject.transform.LookAt(gameObject.transform.position + moveToward);
-		moveToward = gameObject.transform.forward * speed * movementSpeed;
-		gameObject.transform.Translate(moveToward * Time.deltaTime, Space.World);
 		
+		if (characterController.isGrounded) {
+			//// 360 degree
+			if (CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0) {
+				speed = Mathf.Clamp(speed + Time.deltaTime, minSpeed, maxSpeed);
+				animator.SetFloat("WalkSpeed", speed);
+			}
+			else {
+				speed = Mathf.Clamp(speed - Time.deltaTime, minSpeed, maxSpeed);
+				animator.SetFloat("WalkSpeed", speed);
+			}
+
+			moveToward = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), moveToward.y, CrossPlatformInputManager.GetAxis("Vertical"));
+			gameObject.transform.LookAt(gameObject.transform.position + new Vector3(moveToward.x, 0, moveToward.z));
+			
+			moveToward = gameObject.transform.forward * speed * movementSpeed;
+
+			if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+				animator.SetTrigger("Jump");
+				speed = 0;
+				moveToward.y = jumpSpeed;
+				animator.SetFloat("WalkSpeed", speed);
+			}
+		}
 		
+		characterController.Move(moveToward * Time.deltaTime);
 		
 	}
 }
