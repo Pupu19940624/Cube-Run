7329f91e066f54f848051cfb39ab5fd6f3921615
kevin940726 <kevin830726@gmail.com>
2015-11-22 18:07:53 +0800

    remove movement 慣性

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -5,9 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class CharacterMovement : MonoBehaviour {
 
-	public float movementSpeed = 4.0f;
-	public float jumpSpeed = 8.0f;
-	public float gravity = 2.0f;
+	public float movementSpeed = 5.0f;
+	public float jumpSpeed = 5.0f;
 	public static Animator animator;
 	float speed = 0.0f;
 	float maxSpeed = 1.0f;
@@ -41,11 +40,13 @@ public class CharacterMovement : MonoBehaviour {
 		if (characterController.isGrounded) {
 			//// 360 degree
 			if (CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0) {
-				speed = Mathf.Clamp(speed + Time.deltaTime, minSpeed, maxSpeed);
+				speed = 1;
+				// speed = Mathf.Clamp(speed + Time.deltaTime, minSpeed, maxSpeed);
 				animator.SetFloat("WalkSpeed", speed);
 			}
 			else {
-				speed = Mathf.Clamp(speed - Time.deltaTime, minSpeed, maxSpeed);
+				speed = 0;
+				// speed = Mathf.Clamp(speed - Time.deltaTime, minSpeed, maxSpeed);
 				animator.SetFloat("WalkSpeed", speed);
 			}
 
