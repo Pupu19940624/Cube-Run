f3514dff097441fd310e571f182fb162df8cb2d0
kevin940726 <kevin830726@gmail.com>
2015-12-27 14:18:26 +0800

    add loading scene

---
+++ Assets/Scripts/LoadingProgress.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingProgress : MonoBehaviour {
+
+	public AsyncOperation async;
+	public float oy;
+	public float oz;
+
+	IEnumerator Start() {
+		oy = transform.position.y;
+		oz = transform.position.z;
+
+        async = Application.LoadLevelAsync("Scene");
+        yield return async;
+        Debug.Log("Loading complete");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (async != null) {
+			transform.position = new Vector3(-5f + 10 * async.progress, oy, oz);
+		}
+	}
+}
